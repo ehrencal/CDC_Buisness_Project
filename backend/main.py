da5d5b2e2527de62a1c994f98c14d35d735b1a2d
arjbarber <arjbarber@gmail.com>
2025-09-20 15:39:06 -0400

    commmented out template line

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-    #return jsonify({"message": "Welcome to the Flask app!"})
+    #return render_template('index.html')
+    return jsonify({"message": "Welcome to the Flask app!"})
 
 @app.route('/forecast')
 def forcastYears():
